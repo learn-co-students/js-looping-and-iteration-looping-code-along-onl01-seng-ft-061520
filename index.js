098c90a2e47235a025206efbd3cfe6af2bfd4e16
urmajesty <chyrellelewis@gmail.com>
2020-09-15 04:22:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, holiday) {
+	let thanks = [];
+	for (let i = 0; i < arr.length; i++) {
+		thanks.push(`Thank you, ${arr[i]}, for the wonderful ${holiday} gift!`);
+	}
+	return thanks;
+}
+
+function countDown(x) {
+	let int = x;
+	while (int >= 0) {
+		console.log(int--);
+	}
+}
