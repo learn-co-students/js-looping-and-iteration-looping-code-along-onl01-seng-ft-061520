aab8f01d3d2ebf3704d4f9b12a3013df8e05a954
Soundwood <j.benhogan@gmail.com>
2020-09-17 08:25:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function countDown(num){
+    for (let count = num; count > -1; count--){
+        console.log(count)
+    }
+}
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
