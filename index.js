ac21ffb7a51fcba8c8b1365f1e65bef0b09c36e8
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-09-19 14:07:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(friends, eventName) {
+  let emptyAr = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    emptyAr.push(
+      `Thank you, ${friends[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+
+  return emptyAr;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
