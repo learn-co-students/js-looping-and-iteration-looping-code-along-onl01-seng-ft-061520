71a6445865478076ce89ef857e6b7f9a3f8e997c
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-10-02 15:33:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+//let namesArray = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(namesArray, event) {
+    var newArray = []
+    for (let i = 0; i < namesArray.length; i++) {
+        newArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+
+//writeCards(namesArray);
+
+function countDown(count) {
+
+    while (count >= 0) {
+
+        console.log(count);
+        count -= 1;
+    }
+
+    return count;
+}
+countDown();
