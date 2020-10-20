14920da48b9b4facf72b691a66002edd25a904a8
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-10-20 14:28:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, message){
+  let result = []
+  for(let i = 0; i<array.length; i++){
+    result.push(`Thank you, ${array[i]}, for the wonderful ${message} gift!`);
+  };
+  return result;
+}
+
+function countDown(num){
+  for(let i=num; i>=0; i--){
+    console.log(i);
+  };
+}
